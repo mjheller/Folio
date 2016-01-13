2faf48822a39029f459cccee8eb44e17cf15149d
hoelter <hoelterc@gmail.com>
2016-01-13 10:35:00 -0600

    check

---
+++ Folio/src/Folio/SeedData/StockCSVMap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CsvHelper.Configuration;
+using Folio.Models;
+
+namespace Folio.SeedData
+{
+    //public class StockCSVMap : CsvClassMap<Stock>
+    //{
+    //     public StockCSVMap()
+    //    {
+    //        Map(m => m.Symbol).Index(0);
+    //        Map(m => m.Description).Index(17);
+    //    }
+    //}
+}

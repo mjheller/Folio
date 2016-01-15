9b74306d83080762018fb169f1174f09ec2e052f
Matt <mjheller414@gmail.com>
2016-01-15 11:37:26 -0600

    added starter MonteCarlo Controller, ViewModel, View

--- Folio/src/Folio/Services/MonteCarlo/PortfolioPath.cs
+++ Folio/src/Folio/Services/MonteCarlo/PortfolioPath.cs
@@ -18,8 +18,8 @@ namespace Folio.Services.MonteCarlo
         public double folioReturn;
         public double folioStDev;
         public List<double> portfolioValueList;
-        int stepCount = 1;
-        // remember changes made in Stock class with Math.Log etc marked with //add
+        int stepCount = 0;
+        
         public PortfolioPath(int yearsUntilRetirement, double expectedReturn, double variance, double initialPortfolioValue, double annualContribution, double incomeDraw, int yearsPlannedRetirement)
         {
             this.folioReturn = expectedReturn;
@@ -46,7 +46,6 @@ namespace Folio.Services.MonteCarlo
 
         private double ComputePortfolioChange(double currentValue, double randomReturn)//Computes change in value between two points in the path
         {
-
             if (stepCount < retirement)
             {
                 return currentValue * randomReturn + annualContribution;
@@ -55,8 +54,6 @@ namespace Folio.Services.MonteCarlo
             {
                 return currentValue * randomReturn - incomeDraw;
             }
-
-
         }
     }
 }
